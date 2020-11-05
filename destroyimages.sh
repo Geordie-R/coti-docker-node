522d562cc3ddb9f586571dad7b6a9217ff4433dd
GeordieR <40744776+Geordie-R@users.noreply.github.com>
2020-11-05 18:44:33 +0000

    Update destroyimages.sh

--- destroyimages.sh
+++ destroyimages.sh
@@ -1,3 +1,6 @@
+#This is not downloaded by default.  This code is here in case someone wants to stop and removal ALL of their docker images on a VPS.
+#DO NOT RUN THIS IF YOU DO NOT UNDERSTAND THE IMPLICATIONS
+
 docker stop $(docker ps -aq)
 docker rm $(docker ps -aq)
 docker rmi $(docker images -q)
